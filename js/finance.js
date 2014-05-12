b294399fa3622188083068889f3260e4b5481f0a
Padmashree Bhat <padmashreemr@gmail.com>
2014-05-12 23:19:04 +0530

    Data appended in receipt
    1>Data retrieved from JSON file and appended the same in receipt
    2> able to print receipt
    3> Bug found after printing Main menu hangs :-(

--- js/finance.js
+++ js/finance.js
@@ -87,6 +87,49 @@
           
         });
         }
+        
+        
+        
+/*---------------------------------------------------------------------------------        
+------------------------Receipt--------------------------------------------
+----------------------------------------------------------------------------------*/
+function generateReceipt()
+{
+/* Send data to database */
+/* Update Transaction ID */
+                       
+         /*    window.open('Receipt.html', 'newwindow', 'width=300, height=250');
+             
+              $('Receipt').html('<p> Padma: ' + jd.name + '</p>');*/
+              
+              
+              /* Generate receipt */
+			  var receiptdata;
+			  var cont = $('#filler-below');
+				cont.load('finance.html #Receiptwin', function(){
+				console.log("inside finance Receipt");
+				 $.getJSON('JSON/result.json', function(jd) { 
+					 $('#Receiptwin #Receipt #transnum').append(jd.transactionNum);
+					 $('#Receiptwin #Receipt #receiptName').append(jd.fname);
+					 $('#Receiptwin #Receipt #receiptMobile').append(jd.phone);
+				     $('#Receiptwin #Receipt #receiptLandLine').append(jd.fname);
+					 $('#Receiptwin #Receipt #receiptAptNum').append(jd.apartment);
+					 $('#Receiptwin #Receipt #receiptAmt').append(jd.Amount);
+					/*$(jd.transactionNum).appendTo('#Receiptwin #Receipt #transnum');*/
+
+				});
+			/*	
+             $('#Receiptwin').append('<p> Details: ' + jd.Details+ '</p>');*/
+
+				       
+}
+             
+);
+
+				        
+				        
+
+}
 /*---------------------------------------------------------------------------------        
  ------------------------Load Finance--------------------------------------------
 ----------------------------------------------------------------------------------*/
@@ -116,5 +159,4 @@
 					});			          
 				});
         }
-
 

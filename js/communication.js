358f341f7d11d974d4e86dd89ee2c73286344b0c
Padmashree Bhat <padmashreemr@gmail.com>
2014-06-16 14:05:20 +0530

    HomePage with login created
    1> New tab menu added
    2> Login page created

--- js/communication.js
+++ js/communication.js
@@ -1,4 +1,69 @@
 /*---------------------------------------------------------------------------------        
+ ------------------------Load Opinion Poll--------------------------------------------
+----------------------------------------------------------------------------------*/
+ function loadopinionpoll(divID) {
+            var cont = $('#filler-below');
+				cont.load('communication.html #Poll', function(){
+				console.log("inside Poll");			
+
+				$('#Poll #tabscontent #CreatePoll').show();
+				$('#Poll #tabscontent #ViewPoll').hide();
+				
+				cont = $('#Poll #tabs #Create');
+				$(cont).hover(function(e){ 
+					console.log("inside Create");
+					$('#Poll #tabscontent #CreatePoll').show();
+					$('#Poll #tabscontent #ViewPoll').hide();
+					});			          
+				cont = $('#Poll #tabs #View');
+				$(cont).hover(function(e){ 
+					console.log("inside View");
+					$('#Poll #tabscontent #CreatePoll').hide();
+					$('#Poll #tabscontent #ViewPoll').show();
+					createcomplainttable();
+					});			          
+				});
+
+        }
+
+function createpolltable(){
+			     var tablecontents = "";
+		    tablecontents = "<table>";
+		      tablecontents += "<tr>";
+		      tablecontents += "<th>" + "CompliantID" + "</th>";
+		      tablecontents += "<th>" + "Date" + "</th>";
+		      tablecontents += "<th>" + "Subject" + "</th>";
+		      tablecontents += "<th>" + "AssignedTo" + "</th>";
+		      tablecontents += "<th>" + "Status" + "</th>";
+		      tablecontents += "</tr>";
+		
+		/* Read content from JSON file and update the same */
+			  $.getJSON('JSON/result.json', function(jd) { 
+			  for (var i = 0; i < 15; i ++)
+			   {
+			      tablecontents += "<tr>";
+			      tablecontents += "<td>" + jd.transactionNum +"</td>";
+			      tablecontents += "<td>" + jd.date + "</td>";
+			      tablecontents += "<td>" + jd.Remarks + "</td>";
+			      tablecontents += "<td>" + jd.fname+ "</td>";
+			      tablecontents += "<td>" + "Approved" + "</td>";
+			      tablecontents += "<td>" + '<input type="button" class="tablebutton" value = "View" onClick="Javacsript:ViewReq(this)">' + "</td>";
+			      tablecontents += "</tr>";
+			   }
+			   
+			   tablecontents += "</table>";
+			   document.getElementById("complianttable").innerHTML = tablecontents;
+				});
+				
+				$("#transvalue ").click(function(){
+				alert("transaction value clicked");
+});
+
+}
+
+
+
+/*---------------------------------------------------------------------------------        
  ------------------------Load Complaint Box--------------------------------------------
 ----------------------------------------------------------------------------------*/
  function loadcompliantbox(divID) {
@@ -53,6 +118,69 @@ function createcomplainttable(){
 			   
 			   tablecontents += "</table>";
 			   document.getElementById("complianttable").innerHTML = tablecontents;
+				});
+				
+				$("#transvalue ").click(function(){
+				alert("transaction value clicked");
+});
+
+}
+
+
+/*---------------------------------------------------------------------------------        
+ ------------------------Load Notice Board--------------------------------------------
+----------------------------------------------------------------------------------*/
+ function loadnoticeboard(divID) {
+            var cont = $('#filler-below');
+				cont.load('communication.html #NoticeBoard', function(){
+				console.log("inside NoticeBoard");			
+
+				$('#NoticeBoard #tabscontent #CreateNotice').show();
+				$('#NoticeBoard #tabscontent #ViewNotice').hide();
+				
+				cont = $('#NoticeBoard #tabs #Create');
+				$(cont).hover(function(e){ 
+					console.log("inside Create");
+					$('#NoticeBoard #tabscontent #CreateNotice').show();
+					$('#NoticeBoard #tabscontent #ViewNotice').hide();
+					});			          
+				cont = $('#NoticeBoard #tabs #View');
+				$(cont).hover(function(e){ 
+					console.log("inside View");
+					$('#NoticeBoard #tabscontent #CreateNotice').hide();
+					$('#NoticeBoard #tabscontent #ViewNotice').show();
+					createnoticetable();
+					});			          
+				});
+
+        }
+
+function createnoticetable(){
+			     var tablecontents = "";
+		    tablecontents = "<table>";
+		      tablecontents += "<tr>";
+		      tablecontents += "<th>" + "NoticeID" + "</th>";
+			  tablecontents += "<th>" + "Owner" + "</th>";
+		      tablecontents += "<th>" + "Date" + "</th>";
+		      tablecontents += "<th>" + "Subject" + "</th>";
+			  tablecontents += "<th>" + "" + "</th>";
+		      tablecontents += "</tr>";
+		
+		/* Read content from JSON file and update the same */
+			  $.getJSON('JSON/result.json', function(jd) { 
+			  for (var i = 0; i < 15; i ++)
+			   {
+			      tablecontents += "<tr>";
+			      tablecontents += "<td>" + jd.transactionNum +"</td>";
+			      tablecontents += "<td>" + jd.date + "</td>";
+				  tablecontents += "<td>" + jd.fname + "</td>";
+			      tablecontents += "<td>" + jd.Remarks + "</td>";
+				  tablecontents += "<td>" + '<input type="button" class="tablebutton" value = "View" onClick="Javacsript:ViewReq(this)">' + "</td>";
+			      tablecontents += "</tr>";
+			   }
+			   
+			   tablecontents += "</table>";
+			   document.getElementById("noticetable").innerHTML = tablecontents;
 				});
 				
 				$("#transvalue ").click(function(){
